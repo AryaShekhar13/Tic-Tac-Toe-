ee9d1268c703df3f22b600acbc6ff6b25d8c0b4d
Arya Shekhar <kneegrowreal831@gmail.com>
2026-09-03 01:25:03 +0530

    Added 2 Player Alternating functionality

--- main.py
+++ main.py
@@ -38,27 +38,90 @@ GameOver = False
 
 print("1.Play with a Friend\n")
 print("2.Play with the computer\n")
-choice = input()
+choice1 = input()
 
-while choice != "1" and choice != "2":
+while choice1 != "1" and choice1 != "2":
     print("Choose from either 1 or 2")
-    choice = input()
+    choice1 = input()
 
+print("1.Choose X\n")
+print("2.Choose O\n")
+choice2 = input()
+
+while choice2 != "1" and choice2 != "2":
+    print("Choose from either X or O")
+    choice2 = input()
+
+if choice1 == "1":
+    print("1.Choose First to Play\n")
+    print("2.Choose Second to Play\n")
+    choice3 = input()
+
+    while choice3 != "1" and choice3 != "2":
+        print("Choose from either 1 or 2")
+        choice3 = input()
+
+if choice1 == "1":
+    if choice2 == "1" and choice3 == "1" or choice2 == "2" and choice3 == "2":
+        Player1_symbol = "X"
+        Player2_symbol = "O"
+    else:
+        Player1_symbol = "O"
+        Player2_symbol = "X"
+
+if(choice1 == "2"):
+    if(choice2 == "1"): Player_symbol = "X"
+    if(choice2 == "2"): Player_symbol = "O"
+        
 print("This is the values assigned to the box:")
 display(board_values)
 print("Starting the game.....")
 display(board)
 
+Count = 0
+Player1_Turn = True
+Player2_Turn = False
+Comp = False
+if choice1 == "2": Comp = False
+
+print("Choose location to enter:")
 
 while GameOver != True:
-    print("Choose location to enter:")
-    location = input()
-    if Valid(location) == True:
-        row,col = board_map[location]
-        board[row][col]
-        positions_allowed.remove(location)
+    if Count%2 == 0: 
+        Player1_Turn = True
+        Player2_Turn = False
     else:
-        print("Choose a different position!") 
+        Player2_Turn = True
+        Player1_Turn = False
+    location = input()
+    if Player1_Turn == True:
+        print("Player1:\n")
+        if Valid(location) == True:
+            row,col = board_map[location]
+            board[row][col] = Player1_symbol
+            positions_allowed.remove(location)
+            Count+=1
 
-    if not positions_allowed:
-        GameOver = True
+            print("Board:\n")
+            display(board)
+        else:
+            print("Choose a different position!")
+
+        if not positions_allowed:
+            GameOver = True
+
+    if Player2_Turn == True:
+        print("Player1:\n")
+        if Valid(location) == True:
+            row,col = board_map[location]
+            board[row][col] = Player2_symbol
+            positions_allowed.remove(location)
+            Count+=1
+
+            print("Board:\n")
+            display(board)
+        else:
+            print("Choose a different position!")
+
+        if not positions_allowed:
+            GameOver = True
